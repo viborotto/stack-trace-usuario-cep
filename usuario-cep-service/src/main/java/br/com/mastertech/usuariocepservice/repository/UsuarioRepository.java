0f05f16bf82f5df9d754efa3e74edd97bc9d549b
viborotto <vittoriaborotto@gmail.com>
2020-05-14 22:17:29 -0300

    Arrumando

--- usuario-cep-service/src/main/java/br/com/mastertech/usuariocepservice/repository/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.mastertech.usuariocepservice.repository;
-
-import br.com.mastertech.usuariocepservice.model.Usuario;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
-}
